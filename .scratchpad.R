f8abe98fe9fc84f3f0a2b23ae1862ccb5ae48a45
valcu <valcu.mihai@gmail.com>
2014-10-10 17:05:26 +0200

    -

---
+++ .scratchpad.R
@@ -0,0 +1,30 @@
+
+
+
+
+	require(colorZapper)
+	dir = system.file(package = "colorZapper", "sample")
+	CZopen(path = tempfile() )
+	CZaddFiles(dir)
+	
+	# points, all images, no defined marks
+	CZdefine(points = 1)
+	# points, all images, marks
+	CZdefine(points = 1, marks = c("wing", "tail") )
+	# points, selected images
+	CZdefine(points = 2, what = c(1,3) )
+	#selected images & marks
+	CZdefine(points = 2, marks = c("wing", "tail"), what = c(1,3) )
+
+
+	# polygons, all images, no defined marks
+	CZdefine(polygons = 1)
+	# polygons, all images, marks
+	CZdefine(polygons = 1, marks = c("wing", "tail") )
+	# polygons, selected images
+	CZdefine(polygons = 2, what = c(1,3) )
+	#selected images & marks
+	CZdefine(polygons = 2, marks = c("wing", "tail"), what = 4)
+
+
+	CZShowStatus()
--- R/3_interact.R
+++ R/3_interact.R
@@ -1,20 +1,4 @@
 
-czplot <-function(r) {
-	plotRGB (r, maxpixels = Inf)
-	}
-	
-czlocator <-function(type, ... ) {
-	res = locator(...) 
-	if(Sys.getenv("RSTUDIO") == "1") dev.off()
-	
-	switch(type, 
-	p = paste("MULTIPOINT(", paste("(", res$x, res$y, ")", collapse = ","), ")"), 
-	l = NA)
-	
-	
-	}
-	
-	
 #' photo ID-s
 #' Show processed files
 #' examples
@@ -27,11 +11,16 @@ czlocator <-function(type, ... ) {
 CZShowStatus <- function() {
 	stopifnot( colorZapper_file_active())
 	d = dbGetQuery(options()$cz.con, "
-	select distinct f.id, 
-		CASE WHEN instr(wkt, 'MULTIPOINT')	THEN 'points' 
-			WHEN instr(wkt, 'MULTIPOLYGON')	THEN  'polygons' ELSE 'image' END as selected,
-			path fileName
-				from files f left join ROI r on f.id = r.id")
+	SELECT count(id) replicates, id, mark, fileName FROM 
+		(select f.id, mark, 
+			CASE WHEN instr(wkt, 'MULTIPOINT')	THEN 'points' 
+				WHEN instr(wkt, 'MULTIPOLYGON')	THEN  'polygons'  END as selected,
+				path fileName
+					from files f left join ROI r on f.id = r.id )
+	GROUP BY id, mark, fileName  	
+	")
+				
+				
 	d$fileName = gsub("((\\.(?i)(jpg|jpeg|png|gif|bmp|tif|tiff))$)", "", basename(d$fileName) )
 	d
 } 
@@ -48,55 +37,117 @@ CZShowStatus <- function() {
 #' 
 #' }
 
-setGeneric("CZdefine", function(points, polygons, what, ...) standardGeneric("CZdefine") )
+setGeneric("CZdefine", function(points, polygons,  ...) standardGeneric("CZdefine") )
 
 
-# points & no marks
+# points
 setMethod("CZdefine",
-signature = c(points = "numeric", polygons = "missing", what = "missing"),
-	definition = function(points, ...) {
-		stopifnot( colorZapper_file_active())
-		
-	f =  dbGetQuery(options()$cz.con, "select * from files f where f.id not in (select distinct id from ROI where instr(wkt, 'MULTIPOINT') = 1 )")
-
-	if(nrow(f) == 0) stop("You pushed points on all images here.")
+signature = c(points = "numeric", polygons = "missing"),
+	definition = function(points , marks = NA, what, ...) {
+	stopifnot( colorZapper_file_active() )
 	
-	  
-	  for(i in 1:nrow(f) ) {
+	qstr = "select * from files f where f.id not in (select distinct id from ROI where instr(wkt, 'MULTIPOINT') = 1 )"
+	if(!missing(what)) 
+	 qstr = paste("select * from files f where f.id in", paste('(', paste(what, collapse = ","), ')') )
+	
+	f =  dbGetQuery(options()$cz.con, qstr)
+	
+	if(missing(what) & nrow(f) == 0) stop("You pushed points on all images here.")
+		
+	for(i in 1:nrow(f) ) {
 		bi = brick (f[i, 'path'], crs = NA, nl = 3)    
-		czplot(bi)
-		v = czlocator(n = points, type = 'p')		# ...
-		d = data.frame( id = f[i, 'id'], wkt = v, mark = NA, pk = NA)
+		
+		plotRGB (bi, maxpixels = Inf)
+		
+		v = locator(type = "p", n = points * length(marks), ...) 
+			if(Sys.getenv("RSTUDIO") == "1") dev.off()
+		v = paste("MULTIPOINT(", paste("(", v$x, v$y, ")", collapse = ","), ")")
+		
+		d = data.frame( id = f[i, 'id'], wkt = v, mark = rep(marks, each = points), pk = NA)
 		dbWriteTable(options()$cz.con, "ROI", d, row.names = FALSE, append = TRUE)	
 		flush.console() 
 		cat(i, "of", nrow(f), "\n" )
-		}
+	
+	}
+}
+)
+
+# polygons
+setMethod("CZdefine",
+signature = c(points = "missing", polygons = "numeric"),
+	definition = function(polygons , marks = NA, what, ...) {
+	stopifnot( colorZapper_file_active() )
+	
+	qstr = "select * from files f where f.id not in (select distinct id from ROI where instr(wkt, 'MULTIPOLYGON') = 1 )"
+	if(!missing(what)) 
+	 qstr = paste("select * from files f where f.id in", paste('(', paste(what, collapse = ","), ')') )
+	
+	f =  dbGetQuery(options()$cz.con, qstr)
+	
+	if(missing(what) & nrow(f) == 0) stop("You painted polygons on all images here.")
+	
+	
+	for(i in 1:nrow(f) ) {
+		bi = brick (f[i, 'path'], crs = NA, nl = 3)    
+		marksCol = as.numeric(factor(marks))
+		plotRGB (bi, maxpixels = Inf)
+		
+		for(j in 1:length(marks) ) {
+
+			P = vector(mode = "list", length = polygons)
+			
+			nj = 0
+			while(nj < polygons) {
+				nj = nj+1
+				P[[nj]]  = locator(type = "l")
+				}
+			
+			gp = lapply(P, function(p) { list( x = c(p$x, p$x[1]),y = c(p$y, p$y[1]) ) } )
+			gp = sapply(gp, function(p) paste(p$x, p$y, collapse = ",") )
+			gp = paste('((', gp, '))', collapse = ",")
+			gp = paste('MULTIPOLYGON(', gp, ')')
+			
+			plot(readWKT(gp), border = 2, col = adjustcolor(marksCol[j], .2), add = T)
+			
+			d = data.frame( id = f[i, 'id'], wkt = gp, mark = marks[j], pk = NA)
+			dbWriteTable(options()$cz.con, "ROI", d, row.names = FALSE, append = TRUE)	
+			
+			}
+				
+		if(Sys.getenv("RSTUDIO") == "1") dev.off()
+		flush.console() 
+		cat(i, "of", nrow(f), "\n" )
+	
+	}
+
+	
 	
 	}
 )
 
 
 
-
-
-
-
-
-if(FALSE) {
-	require(colorZapper)
-	dir = system.file(package = "colorZapper", "sample")
-	CZopen(path = tempfile() )
-	CZaddFiles(dir)
-	CZdefine(points = 2)
-
-	CZShowStatus()
-
-
-
-	
-	
- }
-
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 

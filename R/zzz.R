a17e3cac17d93f9424326e277741c89cc939699e
valcu <valcu.mihai@gmail.com>
2014-10-13 17:12:36 +0200

    -

--- R/zzz.R
+++ R/zzz.R
@@ -2,12 +2,7 @@
 .onLoad <- function(libname, pkgname) {
 	
 	wd = getwd()
-	# czproj = czExists(wd)
-  czproj = wd
-	
-	options(cz.project = czproj)
-
-	packageStartupMessage( paste("Found colorZapper project", shQuote("foo"), "at", wd) )
+	# packageStartupMessage( paste("Found colorZapper project", shQuote("foo"), "at", wd) )
 	
 	}
 

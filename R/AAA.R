8ac1d625351dc89a8a91949b9901bdebb7b9f791
Mihai Valcu <valcu.mihai@gmail.com>
2018-03-13 23:21:00 +0100

    post R CMD check  fixes

---
+++ R/AAA.R
@@ -0,0 +1,35 @@
+
+
+.onLoad <- function(libname, pkgname) {
+	
+	wd = getwd()
+	# packageStartupMessage( paste("Found colorZapper project", shQuote("foo"), "at", wd) )
+	
+	}
+
+
+
+# data.table, foreach, rangeMapExport 'values'
+# utils::globalVariables(c('i', '.', 'value', 'CON'))
+
+#' @import  data.table  raster rgeos RSQLite sp
+NULL
+
+
+#' @importFrom grDevices    adjustcolor  dev.off  pdf  rgb  rgb2hsv
+NULL
+#' @importFrom graphics     locator  mtext  points
+NULL
+#' @importFrom utils        flush.console  packageVersion
+NULL
+
+
+#' @importFrom parallel   makePSOCKcluster  detectCores stopCluster 
+NULL
+#' @importFrom doParallel  registerDoParallel 
+NULL
+#' @importFrom foreach   %dopar%  foreach registerDoSEQ
+NULL
+#' @importFrom magrittr   %>%  
+NULL
+
--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-
-
-.onLoad <- function(libname, pkgname) {
-	
-	wd = getwd()
-	# packageStartupMessage( paste("Found colorZapper project", shQuote("foo"), "at", wd) )
-	
-	}
